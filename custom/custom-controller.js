9de68e8639627ce4b7c4b8090a802fe9ebe3ce72
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-16 16:09:24 -0500

    finished basic controller to populate the html from the factory. cleaned up the static brainstorming html from custom.html

--- custom/custom-controller.js
+++ custom/custom-controller.js
@@ -1,13 +1,32 @@
 // get database from local storage
 const storedPackages = JSON.parse(localStorage.getItem("packages"));
-const packagesEl = document.getElementById("mainSection");
 
-//loop through nav array
+// finding the html element to add data to below. 
+const packagesElement = document.getElementById("mainSection");
+
+
+// Adding h2 element (loops through the outtermost container array to the first layer of nested objects)
 for (let i = 0; i < storedPackages.length; i++) {
+      
     let currentPackage = storedPackages[i];
 
-    navEl.innerHTML += `
-    <li><a href="${currentPackage.__}">${currentPackage.__}</a></li>
-    `
+    packagesElement.innerHTML +=
+        `<h2>${currentPackage.name}</h2>`
+
+    // Nested for loop to go deeper into the database to find objects nested  within the array nested in the previous loop's currentPackage
+    for (let j = 0; j < currentPackage.services.length; j++)  {
     
+        let currentService = currentPackage.services[j];
+
+        packagesElement.innerHTML +=
+            `<button>
+                <h3>${currentService.name}</h3>
+                <ul>
+                    <li>${currentService.price}</li>
+                    <li>${currentService.description}</li>
+                </ul>
+            </button>
+            `
+    }
+
 }
