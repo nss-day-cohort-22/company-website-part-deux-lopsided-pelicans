9a345e3b9e479a76d5149c549aa7f3c82861ebbd
Ray Medrano <rmbw74@gmail.com>
2017-10-17 11:48:42 -0500

    added some basic validation

--- testimonials/js/testimonials-validate.js
+++ testimonials/js/testimonials-validate.js
@@ -1,11 +1,23 @@
-function checkForBlank() {
+function clicker() {
     // select the 3 form entry fields and get value
     let checkName = document.forms["submission-form"]["fullname"].value;
     let checkMail = document.forms["submission-form"]["email"].value;
     let checkDescrip = document.forms["submission-form"]["description"].value;
 
     if (checkName == "" || checkMail == "" || checkDescrip =="") {
-        alert("Please fill all fields before submit");
         return false;
+    }else {
+        clickershow();
     }
+}
+function clickershow() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar")
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 5 seconds, remove the show class from DIV
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    return true;
 }
