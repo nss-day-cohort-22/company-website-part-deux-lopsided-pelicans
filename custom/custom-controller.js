022efc2f425c9cb3d735402d530f42b6dd05e71e
Max Wolf <maxwolf@Maxs-MacBook-Pro.local>
2017-10-17 15:02:29 -0500

    basic funtional version of pagified custom products page with prev, next, 1-5 buttons and all services displayed properly

--- custom/custom-controller.js
+++ custom/custom-controller.js
@@ -5,30 +5,31 @@ const storedPackages = JSON.parse(localStorage.getItem("packages"));
 const packagesElement = document.getElementById("mainSection");
 
 
-// Adding h2 element (loops through the outtermost container array to the first layer of nested objects)
-for (let i = 0; i < storedPackages.length; i++) {
-      
-    let currentPackage = storedPackages[i];
+    // Adding h2 element (loops through the outtermost container array to the first layer of nested objects)
+    for (let i = 0; i < storedPackages.length; i++) {
+        
+        let currentPackage = storedPackages[i];
 
 
-    // Nested for loop to go deeper into the database to find objects nested  within the array nested in the previous loop's currentPackage
-    // NOTE: currentPackage.services.length was not used because i only want to display the first 3 of all the services.
-    for (let j = 0; j < 3; j++)  {
-    
-        let currentService = currentPackage.services[j];
+        // Nested for loop to go deeper into the database to find objects nested  within the array nested in the previous loop's currentPackage
+        // NOTE: currentPackage.services.length was not used because i only want to display the first 3 of all the services.
+        for (let j = 0; j < currentPackage.services.length; j++)  {
+        
+            let currentService = currentPackage.services[j];
 
-        packagesElement.innerHTML +=
-            `<div onclick='toastIt()' class='serviceButton'>
-                <h3>${currentService.name}</h3>
-                <ul>
-                    <li>${currentService.price}</li>
-                    <li>${currentService.description}</li>
-                </ul>
-            </div>
-            `
-            console.log(packagesElement);
-        }
-}
+            packagesElement.innerHTML +=
+                `<div onclick='toastIt()' class='serviceButton'>
+                    <h3>${currentService.name}</h3>
+                    <h4>${currentService.packageTier}</h4>
+                    <ul>
+                        <li>${currentService.price}</li>
+                        <li>${currentService.description}</li>
+                    </ul>
+                </div>
+                `
+                console.log(packagesElement);
+            }
+    }
 
 
 
