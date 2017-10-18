65353ae8561551e70e9294e4d68e4588a2d0799e
Ray Medrano <rmbw74@gmail.com>
2017-10-17 21:39:00 -0500

    added navbar and fixed relative links in factory

--- testimonials/js/testimonials-factory.js
+++ testimonials/js/testimonials-factory.js
@@ -1,19 +1,19 @@
 // testimonial entry objects for database - holding image links and any metadata thats going to be used
 const testimonial1 = {
     "caption":"caption 1", 
-    "image":"/images/img1.jpg"
+    "image":"./images/img1.jpg"
 }
 const testimonial2 = {
     "caption":"caption 2", 
-    "image":"/images/img2.jpg"
+    "image":"./images/img2.jpg"
 }
 const testimonial3 = {
     "caption":"caption 3", 
-    "image":"/images/img3.jpg"
+    "image":"./images/img3.jpg"
 };
 const testimonial4 = {
     "caption":"caption 4", 
-    "image":"/images/img4.jpg"
+    "image":"./images/img4.jpg"
 }
 
 const testimonialDatabase = [] // Create array for testimonial object database
